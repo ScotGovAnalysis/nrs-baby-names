857b553b1baeb307a299745856c6986b56e60aca
Joseph Adams <joseph@josephadams.net>
2019-11-14 17:00:00 +0000

    Set up mouseflow with cookie control

--- www/cookie_control_config.js
+++ www/cookie_control_config.js
@@ -11,7 +11,7 @@ var config = {
             name : 'analytics',
             label: 'Analytical Cookies',
             description: 'Analytical cookies help us to improve our website by collecting and reporting information on its usage.',
-            cookies: ['_ga', '_gid', '_gat', '__utma', '__utmt', '__utmb', '__utmc', '__utmz', '__utmv'],
+            cookies: ['_ga', '_gid', '_gat', '__utma', '__utmt', '__utmb', '__utmc', '__utmz', '__utmv', 'mf_user'],
             recommendedState: true,
             onAccept : function(){
                 // Add Google Analytics
@@ -20,11 +20,25 @@ var config = {
                 gtag('js', new Date());
                 gtag('config', 'UA-91956629-1', { 'anonymize_ip':true });
                 // End Google Analytics
+                
+                // Add mouseflow
+                window._mfq = window._mfq || [];
+                (function() {
+                    var mf = document.createElement("script");
+                    mf.type = "text/javascript"; mf.async = true;
+                    mf.src = "//cdn.mouseflow.com/projects/0b5cc4f0-f12e-4e86-818b-a9b648d9f1a3.js";
+                    document.getElementsByTagName("head")[0].appendChild(mf);
+                })();
+                // End mouseflow
             },
             onRevoke: function(){
                 // Disable Google Analytics
                 window['ga-disable-UA-91956629-1'] = true;
                 // End Google Analytics
+                
+                // Stop mouseflow recording
+                mouseflow && mouseflow.stopSession();
+                // End mouseflow
             }
         }
     ]
--- www/mouseflow.js
+++
@@ -1,7 +0,0 @@
-window._mfq = window._mfq || [];
-(function() {
-  var mf = document.createElement("script");
-  mf.type = "text/javascript"; mf.async = true;
-  mf.src = "//cdn.mouseflow.com/projects/2146462b-0ea5-4603-916d-9a03026cb9e7.js";
-  document.getElementsByTagName("head")[0].appendChild(mf);
-})();
