9cc73c9bc1b3abd2ccc18efab50f1e3156c6c4fd
Rhi <rbatstone94@gmail.com>
2021-03-30 15:20:09 +0100

    Update file name

--- create_data.R
+++ create_data.R
@@ -4,7 +4,7 @@ library(tidyr)
 library(feather)
 
 babynames <- read_csv(
-  file = "www/Babies First Names - all names - all years 2.csv") %>%
+  file = "www/Babies First Names - all names - all years.csv") %>%
   select(yr, sex, FirstForename, number) %>%
   transmute(firstname = as.character(FirstForename),
             sex = case_when(sex == "B" ~ "Male",
