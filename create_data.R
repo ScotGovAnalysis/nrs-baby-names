c42563f496cb81edf2eebbacad1c9ade56260a29
Rhi Batstone <60016148+rhi-batstone@users.noreply.github.com>
2023-03-21 14:43:18 +0000

    Update create_data.R
    Update this script to to use modern function 
    i.e.: 
    transmute() -> mutate()
    spread -> pivot_wider()
    Also, now catches duplicate rows

--- create_data.R
+++ create_data.R
@@ -5,13 +5,15 @@ library(feather)
 
 babynames <- read_csv(
   file = "www/Babies First Names - all names - all years.csv") %>%
-  select(yr, sex, FirstForename, number) %>%
-  transmute(firstname = as.character(FirstForename),
-            sex = case_when(sex == "B" ~ "Male",
-                            sex == "G" ~ "Female") %>%
-              as.factor(),
-            year = yr,
-            number = number) %>%
-  spread(key = year, value = number, fill = 0)
+  select("year" = yr, sex, "firstname" = FirstForename, number) %>%
+  mutate(firstname = as.character(firstname),
+         sex = as.factor(case_when(sex == "B" ~ "Male",
+                         sex == "G" ~ "Female"))) %>%
+  distinct(.keep_all = TRUE) %>% 
+  pivot_wider(names_from = year, 
+              values_from = number, 
+             values_fill = 0)
+
+
 
 write_feather(babynames, "www/data_all.feather")
