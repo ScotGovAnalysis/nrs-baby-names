2c5554bbdf0f7245ceb5f056304400e82334bc38
Rhi Batstone <60016148+rhi-batstone@users.noreply.github.com>
2023-08-15 12:47:17 +0100

    Update create_data.R to bind historical names

--- create_data.R
+++ create_data.R
@@ -1,17 +1,25 @@
 library(dplyr)
 library(readr)
 library(tidyr)
+library(stringr)
 library(feather)
+
 
 babynames <- read_csv(
   file = "www/Babies First Names - all names - all years.csv") %>%
-  select(yr, sex, FirstForename, number) %>%
+  rbind(readxl::read_xlsx("www/FINAL_top_100_1935_1973.xlsx")) %>% 
+  select(yr, sex, FirstForename, number, rank) %>%
   transmute(firstname = as.character(FirstForename),
-            sex = case_when(sex == "B" ~ "Male",
-                            sex == "G" ~ "Female") %>%
+            sex = case_when(sex %in% c("B", "Boy") ~ "Male",
+                            sex %in% c("G", "Girl") ~ "Female",
+                            TRUE ~ sex) %>%
               as.factor(),
             year = yr,
-            number = number) %>%
-  spread(key = year, value = number, fill = 0)
+            number = number,
+            rank = rank) %>%
+  arrange(year, rank) %>% 
+  pivot_wider(names_from = year, values_from = c(number, rank), values_fill = 0) %>% 
+  rename_with(~str_remove(., 'number_'))
 
 write_feather(babynames, "www/data_all.feather")
+
